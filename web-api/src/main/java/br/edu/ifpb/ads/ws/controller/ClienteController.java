6d389be5646cb5621bc701f61ca5b8b105a89efb
Franck Aragão <fjunior.aragao@gmail.com>
2016-10-08 15:30:16 -0300

    response in POST

--- web-api/src/main/java/br/edu/ifpb/ads/ws/controller/ClienteController.java
+++ web-api/src/main/java/br/edu/ifpb/ads/ws/controller/ClienteController.java
@@ -1,6 +1,8 @@
 package br.edu.ifpb.ads.ws.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,7 +19,11 @@ import br.edu.ifpb.ads.model.Cliente;
 public class ClienteController {
 
 	@RequestMapping(method = RequestMethod.POST, value = "/clientes", consumes=MediaType.APPLICATION_JSON_VALUE)
-	public void cadastrarCliente(@RequestBody Cliente cliente){
-		System.out.println(cliente);
+	public ResponseEntity<Cliente> cadastrarCliente(@RequestBody Cliente cliente){
+			
+		// salva no DB
+		cliente.setId(1l);
+		
+		return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
 	}
 }
