a26e02b32eb89d16751f82ca2c89294b1a7375ad
Franck Aragão <fjunior.aragao@gmail.com>
2016-10-09 01:37:19 -0300

    response in POST

--- web-api/src/main/java/br/edu/ifpb/ads/model/Cliente.java
+++ web-api/src/main/java/br/edu/ifpb/ads/model/Cliente.java
@@ -10,6 +10,14 @@ public class Cliente {
 	private Long id;
 	private String nome;
 
+	public Cliente() {
+	}
+
+	public Cliente(Long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
 	public Long getId() {
 		return id;
 	}
--- web-api/src/main/java/br/edu/ifpb/ads/ws/controller/ClienteController.java
+++ web-api/src/main/java/br/edu/ifpb/ads/ws/controller/ClienteController.java
@@ -1,5 +1,8 @@
 package br.edu.ifpb.ads.ws.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -19,11 +22,21 @@ import br.edu.ifpb.ads.model.Cliente;
 public class ClienteController {
 
 	@RequestMapping(method = RequestMethod.POST, value = "/clientes", consumes=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Cliente> cadastrarCliente(@RequestBody Cliente cliente){
+	public ResponseEntity<Cliente> saveCliente(@RequestBody Cliente cliente){
 			
 		// salva no DB
 		cliente.setId(1l);
 		
 		return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/clientes", produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Cliente>> getAll(){
+		
+		List<Cliente> listClientes = new ArrayList<>();
+		listClientes.add(new Cliente(1l, "João"));
+		listClientes.add(new Cliente(2l, "maria"));
+		
+		return new ResponseEntity<>(listClientes, HttpStatus.OK);
 	}
 }
