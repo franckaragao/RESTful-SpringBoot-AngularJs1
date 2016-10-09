d7e80f7b0048b58e2c6c0a0daf0d172da08e7687
Franck Aragão <fjunior.aragao@gmail.com>
2016-10-09 20:46:29 -0300

    dependencia do angular

---
+++ web-api/src/main/java/br/edu/ifpb/ads/ws/controller/IndexController.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.ads.ws.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String loadIndex(){
+		return "index";
+	}
+}
