7fc6fc1b60ab976d38a81af8ba4814744dd2020f
Franck Aragão <fjunior.aragao@gmail.com>
2016-10-13 10:48:28 -0300

    exclusão

--- web-api/src/main/resources/static/js/app.js
+++ web-api/src/main/resources/static/js/app.js
@@ -33,6 +33,22 @@ appClient.controller('indexController', function($scope, $http) {
 
 		});
 	};
+	
+	$scope.removerCliente = function(cliente){
+		$http({
+			method: 'DELETE',
+			url: 'http://localhost:8080/clientes/'+cliente.id,
+
+		}).then(function onSuccessCallBack(response){
+			
+			var index = $scope.clientes.indexOf(cliente);
+			$scope.clientes.splice(index, 1);
+		},
+
+		function onErrorCallBack(response){
+
+		});
+	};
 	getClientes();
 
 });
